cfc36983914aeb7b4bd8438db50a49ba719f0ae7
oxyflour <oxyflour@hotmail.com>
2017-01-28 11:59:32 +0800

    update publish script

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,21 +1,28 @@
 BUILD_HASH=`webpack | grep Hash: | awk '{ print $2 }'`
-DST_DIR=qcs.ofr.me:~/www/alice-aqua-editor-$BUILD_HASH
+DST_DIR=qcs.ofr.me:~/www/alice-aqua-$BUILD_HASH
 TMP_DIR=.depoly-temp
 SRC_DIRS=(
-  "node_modules/babylonjs/dist/*.js"
+  "babylonjs/*.js"
+  "babylonjs/**/*.js"
   "node_modules/font-awesome/css/*.css"
   "node_modules/font-awesome/fonts/*.*"
 )
 
 mkdir -p $TMP_DIR &&\
-sed -i -e "s/{{BUILD_HASH}}/$BUILD_HASH/" editor.html &&\
+sed -i -r \
+  -e "s/\\{\\{BUILD_HASH\\}\\}/$BUILD_HASH/" \
+  -e "s/<([^>]+)(class=\"build-hash\"[^>]*)>[^<]*</<\1\2>$BUILD_HASH</" \
+  *.html &&\
 cp *.html $TMP_DIR/ &&\
 cp -r assets $TMP_DIR/ &&\
 cp -r build $TMP_DIR/ &&\
 for src in "${SRC_DIRS[@]}"
 do
-  dst=$TMP_DIR/`dirname $src | head -1`
-  mkdir -p $dst && cp -r $src $dst
+  for file in $src
+  do
+    dst=$TMP_DIR/`dirname $file`
+    mkdir -p $dst && cp -r $src $dst
+  done
 done &&\
 rsync -avr $TMP_DIR/. $DST_DIR &&\
 rm -rf $TMP_DIR &&\
